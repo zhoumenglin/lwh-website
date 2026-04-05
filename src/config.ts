7b0af9b967ca65aadaaff247bfb91cfb7d1d79c5
zhoumenglin <zhoumenglin007@gmail.com>
2026-04-05 21:52:16 +0800

    feat: 修改名字从李文慧变成梨贝拉

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "李文慧 | AI 产品经理";
+export const SITE_TITLE = "梨贝拉 | AI 产品经理";
 export const SITE_DESCRIPTION = "6年C端增长+商业化实战经验，正在将增长方法论带入AI产品赛道";
 export const TRANSITION_API = true;
 export const GENERATE_SLUG_FROM_TITLE = false;
